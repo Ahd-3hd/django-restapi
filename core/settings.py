a2067a8b5683fd11d1f8a9d10f501dcfa962a64f
ahd <ahdhani0@gmail.com>
2021-03-07 00:13:30 +0300

    update global permission to AllowAny

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ]
 }
 
