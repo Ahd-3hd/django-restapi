5bf7c5085b2daa7a13071c3c8210179a9df0ff80
ahd <ahdhani0@gmail.com>
2021-03-06 21:09:42 +0300

    display data on react client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,39 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Post from "./components/Post";
-import { useEffect } from "react";
+import Posts from "./components/Posts";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [state, setState] = useState({
+    loading: false,
+    posts: [],
+  });
+
   useEffect(() => {
-    const apiUrl = "http://127.0.0.1:8000/api/";
+    setState((prev) => ({ ...prev, loading: true }));
+    const apiUrl = `http://127.0.0.1:8000/api/`;
     fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .then((data) => data.json())
+      .then((posts) =>
+        setState({
+          posts,
+          loading: false,
+        })
+      )
+      .catch((err) => setState((prev) => ({ ...prev, loading: false })));
   }, []);
   return (
     <Router>
       <Header />
       <Switch>
-        <Route exact path="/" component={Post} />
+        <Route
+          exact
+          path="/"
+          component={() => (
+            <Posts posts={state.posts} isLoading={state.loading} />
+          )}
+        />
       </Switch>
       <Footer />
     </Router>
---
+++ client/src/components/Posts.tsx
@@ -0,0 +1,96 @@
+import PostsLoading from "./PostsLoading";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+interface Post {
+  id: number;
+  title: string;
+  author: string;
+  excerpt: string;
+  content: string;
+  status: string;
+}
+
+const useStyles = makeStyles((theme) => ({
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  postTitle: {
+    fontSize: "16px",
+    textAlign: "left",
+  },
+  postText: {
+    display: "flex",
+    justifyContent: "left",
+    alignItems: "baseline",
+    fontSize: "12px",
+    textAlign: "left",
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+export default function Posts({
+  posts,
+  isLoading,
+}: {
+  posts: Post[];
+  isLoading: boolean;
+}) {
+  const classes = useStyles();
+
+  if (isLoading) return <PostsLoading />;
+  return (
+    <>
+      <Container maxWidth="md" component="main" style={{ marginTop: "2rem" }}>
+        <Grid container spacing={5} alignItems="flex-end">
+          {posts.map((post) => {
+            return (
+              <Grid item key={post.id} xs={12} md={4}>
+                <Card>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="h2"
+                      className={classes.postTitle}
+                    >
+                      {post.title.substr(0, 50)}...
+                    </Typography>
+                    <div className={classes.postText}>
+                      <Typography
+                        component="p"
+                        color="textPrimary"
+                      ></Typography>
+                      <Typography variant="subtitle2" color="textSecondary">
+                        {post.excerpt.substr(0, 60)}...
+                      </Typography>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </>
+  );
+}
