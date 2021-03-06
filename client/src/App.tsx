483d4f0ebd9699658b4a373a0ef3ee383702e41b
ahd <ahdhani0@gmail.com>
2021-03-06 19:58:13 +0300

    fix cors errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,16 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Post from "./components/Post";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const apiUrl = "http://127.0.0.1:8000/api/";
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Router>
       <Header />
