39118e538a1a1ff17bf235970beb182a6ce2e2d9
ahd <ahdhani0@gmail.com>
2021-03-07 17:45:34 +0300

    filter against user

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,63 +1,75 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import SAFE_METHODS, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, BasePermission,IsAuthenticated
+from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
 from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import filters
 from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import filters
+
 
 class PostUserWritePermission(BasePermission):
-    message = "Editing posts is restricted to the author only."
-    def has_object_permission(self,request,view,obj):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+
         if request.method in SAFE_METHODS:
             return True
-        return obj.author == request.user           
 
+        return obj.author == request.user
 
-
-class PostList(viewsets.ModelViewSet):
-    permission_classes = [PostUserWritePermission]
-    serializer_class = PostSerializer
-    # queryset = Post.postobjects.all()
-
-    def get_object(self,queryset=None,**kwargs):
-        item = self.kwargs.get('pk')
-        return get_object_or_404(Post,slug=item)
-
-    # custom Queryset
-    def get_queryset(self):
-        return Post.objects.all()
-
-
-
-
-
-# class PostList(viewsets.ViewSet):
+# User Filter
+# class PostList(generics.ListAPIView):
 #     permission_classes = [IsAuthenticated]
-#     queryset = Post.postobjects.all()
-
-#     def list(self,request):
-#         serializer_class = PostSerializer(self.queryset,many=True)
-#         return Response(serializer_class.data)
-    
-#     def retrieve(self,request,pk=None):
-#         post = get_object_or_404(self.queryset,pk=pk)
-#         serializer_class = PostSerializer(post)
-#         return Response(serializer_class.data)
-
-
-
-
-
-
-    #list, create, retrieve, update, partial_update, destroy
-
-# class PostList(generics.ListCreateAPIView):
-#     permission_classes = [IsAuthenticatedOrReadOnly]
-#     queryset = Post.postobjects.all()
 #     serializer_class = PostSerializer
 
-# class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
-#     permission_classes = [PostUserWritePermission]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+#     def get_queryset(self):
+#         user = self.request.user
+#         return Post.objects.filter(author=user)
+
+class PostList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author=user)
+
+class PostDetail(generics.RetrieveAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        slug = self.request.query_params.get('slug', None)
+        print(slug)
+        return Post.objects.filter(slug=slug)
+
+class PostListDetailfilter(generics.ListAPIView):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^slug']
+
+    # '^' Starts-with search.
+    # '=' Exact matches.
+    # '@' Full-text search. (Currently only supported Django's PostgreSQL backend.)
+    # '$' Regex search.
+
+
+class PostSearch(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^slug']
+
+    # def get_object(self, queryset=None, **kwargs):
+    #     item = self.kwargs.get('pk')
+    #     return get_object_or_404(Post, slug=item)
+
+    # Define Custom Queryset
+    # def get_queryset(self):
+    #     return Post.objects.all()
+
